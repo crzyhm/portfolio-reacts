a1d2ed4de695db76145afba644c30346b6d76cdf
crzyhm <akbarmuh403@gmail.com>
2024-08-15 22:18:39 +0900

    feat-skeltoon

---
+++ src/component/SkeletonLoader.js
@@ -0,0 +1,11 @@
+// SkeletonLoader.js
+import React from 'react';
+import './SkeletonLoader.css'; // Buat file CSS untuk skeleton loader
+
+const SkeletonLoader = () => (
+  <div className="skeleton-loader ">
+
+  </div>
+);
+
+export default SkeletonLoader;
